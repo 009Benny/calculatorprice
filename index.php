999adbce2c863648fd67ec4f7dcb2240e93e7ea7
Benny <009bennyreyes@gmail.com>
2018-12-27 16:18:38 -0600

    avance de video

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Slider RapidCredit</title>
+    <link rel='stylesheet' id='main-css'  href='css/main.css' type='text/css' media='all'/>
+  </head>
+  <body>
+    <div class="center-content">
+      <form class="form-content" action="index.html" method="post">
+        <div class="title">
+          <h1>Calcula tu RapidCredit</h1>
+        </div>
+        <div class="input-content">
+          <div class="money-container">
+            <span>¿Cuánto dinero necesitas?</span>
+            <div class="price-container">$0</div>
+            <div class="minus"><p>-</p></div>
+            <input type="range" min="1" max="100" value="50">
+            <div class="sum"><p>+</p></div>
+          </div>
+          <div class="days-container">
+            <span>¿Cuándo puedes pagarlo?</span>
+            <div class="price-container">$0</div>
+            <div class="minus"><p>-</p></div>
+            <input type="range" class="days" name="days" min="110.000" max="750.000" value="110.000">
+            <div class="sum"><p>+</p></div>
+          </div>
+        </div>
+        <div class="data-display">
+
+        </div>
+        <div class="button-content">
+
+        </div>
+      </form>
+    </div>
+  </body>
+</html>
